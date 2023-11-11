261ed74f9890aefd45c05c0cf19ac982c05e394b
vivekpal1 <31vivekpal@gmail.com>
2023-11-12 05:02:18 +0530

    md/guides

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,19 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import svelte from '@astrojs/svelte';
-
+import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), svelte()],
+  markdown: {
+    syntaxHighlight: 'prism',
+    gfm: true,
+  },
+  integrations: [tailwind(), react(), svelte(),
+    mdx({
+      syntaxHighlight: 'shiki',
+      shikiConfig: { theme: 'dracula' },
+      remarkRehype: { footnoteLabel: 'Footnotes' },
+      gfm: false,
+    }),
+  ],
 });
